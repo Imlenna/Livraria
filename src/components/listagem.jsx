e717f7e2d4461f6b7460ee4bec36496d1a1f0140
Imlenna <milenatrindade.mt@gmail.com>
2021-11-16 13:37:53 -0300

    commit on main

---
+++ src/components/listagem.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Table } from 'react-bootstrap'
+
+const listagem = () => {
+    return (
+        <>
+            <Table size="sm" striped variant="light">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </Table>
+        </>
+    )
+}
+
+export default listagem
--- src/pages/autor/autor.jsx
+++ src/pages/autor/autor.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Listagem from '../../components/listagem'
 
 const autor = () => {
     return (
         <>
            <h1>teste</h1>
            <p>a</p>
+           <Listagem/>
         </>
     )
 }
